2dbf710b78b9eae45f8ea44e25b13d5465835336
Crowdbotics <team@crowdbotics.com>
2021-07-09 03:56:54 +0000

    Update models

--- backend/tcg_project_28650/wsgi.py
+++ backend/tcg_project_28650/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tcg_project_28650.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tcg_project_28650.settings")
 
 application = get_wsgi_application()
